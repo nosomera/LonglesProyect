226da733be813d314e4fa7c182abb96108cdfede
nosomera <camilonauta_22@hotmail.com>
2026-07-28 01:00:51 -0500

    se a{ade links

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,16 @@ const App: React.FC = () => {
 
   const videoUrl7: string = "https://firebasestorage.googleapis.com/v0/b/loveuts-7a90a.firebasestorage.app/o/V3RMG2D_5958_87302_4_y4n1-n3k0.mp4?alt=media&token=3b6d15ff-3962-4874-aa19-9f2511088440";
 
+//--------------------------------------------------------------//
+  const fantasma1: string = "https://firebasestorage.googleapis.com/v0/b/loveuts-7a90a.firebasestorage.app/o/Ushiro%20no%20shonen%20kamui-san%20%2001.mp4?alt=media&token=33211a77-045f-47f0-b8a6-deefce6c3e2c";
+  const fantasma2: string = "https://firebasestorage.googleapis.com/v0/b/loveuts-7a90a.firebasestorage.app/o/Ushiro%20no%20shonen%20kamui-san%20%2002.mp4?alt=media&token=34659e24-1929-4657-ba5d-7fd03577840d";
+  const fantasma3: string = "https://firebasestorage.googleapis.com/v0/b/loveuts-7a90a.firebasestorage.app/o/Ushiro%20no%20shonen%20kamui-san%20%2003.mp4?alt=media&token=bde004b1-c89c-49ba-b294-555a643457de";
+  const fantasma4: string = "https://firebasestorage.googleapis.com/v0/b/loveuts-7a90a.firebasestorage.app/o/Ushiro%20no%20shonen%20kamui-san%20%2004.mp4?alt=media&token=8dbba634-ce28-45ae-a797-8a8f081b899f";
+  const fantasma5: string = "";
+  const fantasma6: string = "";
+  const fantasma7: string = "";
+
+
   return (
     <div className="terror-container">
       
@@ -76,6 +86,63 @@ const App: React.FC = () => {
         </main>
       </section>
 
+
+
+
+
+
+
+    <section className="terror-section">
+        <header className="terror-header">
+          <h1>Ushiro no shonen Kamui/san 01</h1> {/* Puedes cambiar este título */}
+        </header>
+        <main className="terror-main">
+          <div className="video-wrapper">
+            <video src={fantasma1} controls controlsList="nodownload" preload="metadata">
+              Tu navegador no soporta el video.
+            </video>
+          </div>
+        </main>
+      </section>
+
+      <section className="terror-section">
+        <header className="terror-header">
+          <h1>Ushiro no shonen Kamui/san 02</h1> {/* Puedes cambiar este título */}
+        </header>
+        <main className="terror-main">
+          <div className="video-wrapper">
+            <video src={fantasma2} controls controlsList="nodownload" preload="metadata">
+              Tu navegador no soporta el video.
+            </video>
+          </div>
+        </main>
+      </section>
+
+      <section className="terror-section">
+        <header className="terror-header">
+          <h1>Ushiro no shonen Kamui/san 03</h1> {/* Puedes cambiar este título */}
+        </header>
+        <main className="terror-main">
+          <div className="video-wrapper">
+            <video src={fantasma3} controls controlsList="nodownload" preload="metadata">
+              Tu navegador no soporta el video.
+            </video>
+          </div>
+        </main>
+      </section>
+
+      <section className="terror-section">
+        <header className="terror-header">
+          <h1>Ushiro no shonen Kamui/san 04</h1> {/* Puedes cambiar este título */}
+        </header>
+        <main className="terror-main">
+          <div className="video-wrapper">
+            <video src={fantasma4} controls controlsList="nodownload" preload="metadata">
+              Tu navegador no soporta el video.
+            </video>
+          </div>
+        </main>
+      </section>
 
     </div>
   );
